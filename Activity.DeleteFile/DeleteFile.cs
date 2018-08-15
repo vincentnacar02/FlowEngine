fd02c84fa29f2ec4e52d9c0bf1b43689d848b267
vincent <vincentnacar02@gmail.com>
2018-08-16 07:50:00 +0800

    added DeleteFile activity

---
+++ Activity.DeleteFile/DeleteFile.cs
@@ -0,0 +1,39 @@
+﻿using FlowEngine.Core;
+using FlowEngine.SDK.interfaces;
+using FlowEngine.SDK.types;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteFile
+{
+    public class DeleteFile : Activity
+    {
+        public DeleteFile(object Id, IProperties props)
+            : base(Id, props)
+        {
+
+        }
+
+        public override IResult run()
+        {
+            try
+            {
+                var filePathProp = this.getProperties().getProperty("FilePath");
+                if (filePathProp != null)
+                {
+                    String filePath = (String) filePathProp.getValue();
+                    File.Delete(filePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new ActivityResult(null, ResultStatus.HAS_ERROR, ex);
+            }
+            return new ActivityResult(null, ResultStatus.SUCCESS);
+        }
+    }
+}
