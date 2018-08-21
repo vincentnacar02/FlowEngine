aa0abe5f24b342b1b4a60b95a638312ea83dac7c
vincent <vincentnacar02@gmail.com>
2018-08-22 07:47:20 +0800

    added FlowEngine.Service

---
+++ FlowEngine.Service/EngineService.svc.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FlowEngine.Service
+{
+    public class EngineService : IEngineService
+    {
+        public void StartWorkflow(Workflow workflow)
+        {
+            try
+            {
+                //TEST
+                string flowEngine = System.Configuration.ConfigurationManager.AppSettings["WorkflowEngineExe"];
+                string workflowDir = System.Configuration.ConfigurationManager.AppSettings["WorkflowDirectory"];
+                string workflowPath = System.IO.Path.Combine(workflowDir, "testrepeatworkflow.xml");
+
+                System.Diagnostics.ProcessStartInfo workflowInfo = new System.Diagnostics.ProcessStartInfo(flowEngine);
+                workflowInfo.Arguments = "\"" + workflowPath + "\"";
+                workflowInfo.UseShellExecute = false;
+
+                System.Diagnostics.Process.Start(workflowInfo);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public IList<Workflow> GetWorkflows()
+        {
+            Workflow test = new Workflow();
+            List<Workflow> tests = new List<Workflow>();
+            tests.Add(test);
+            return tests;
+        }
+    }
+}
---
+++ FlowEngine.Service/IEngineService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FlowEngine.Service
+{
+    [ServiceContract]
+    public interface IEngineService
+    {
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "start")]
+        void StartWorkflow(Workflow workflow);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "workflows")]
+        IList<Workflow> GetWorkflows();
+    }
+
+
+    [DataContract]
+    public class Workflow
+    {
+        [DataMember]
+        public bool WorkflowId
+        {
+            get;
+            set;
+        }
+    }
+}
